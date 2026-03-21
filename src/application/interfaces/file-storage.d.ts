203ec48cc42c2dfae08244790062801b2b624eca
Safwan Parkar <self@safwanyp.com>
2026-03-21 18:07:51 +0100

    feat(application): add file storage interface

---
+++ src/application/interfaces/file-storage.d.ts
@@ -0,0 +1,48 @@
+interface FileStorageEntry {
+  path: string;
+  name: string;
+  mimeType: string;
+  sizeInBytes: number;
+}
+
+interface SaveFileInput {
+  path: string;
+  name?: string;
+  mimeType?: string;
+  contents: Uint8Array;
+}
+
+interface ReadFileInput {
+  path: string;
+}
+
+interface DeleteFileInput {
+  path: string;
+}
+
+interface MoveFileInput {
+  fromPath: string;
+  toPath: string;
+}
+
+interface FileExistsInput {
+  path: string;
+}
+
+type SaveFile = ({ file }: { file: SaveFileInput }) => FileStorageEntry;
+type ReadFile = ({ file }: { file: ReadFileInput }) => Uint8Array;
+type DeleteFile = ({ file }: { file: DeleteFileInput }) => { success: boolean };
+type MoveFile = ({ file }: { file: MoveFileInput }) => FileStorageEntry;
+type FileExists = ({ file }: { file: FileExistsInput }) => boolean;
+
+interface FileStorage {
+  saveFile: SaveFile;
+  readFile: ReadFile;
+  deleteFile: DeleteFile;
+  moveFile: MoveFile;
+  fileExists: FileExists;
+}
+
+type CreateFileStorage = () => FileStorage;
+
+export type { FileStorageEntry, FileStorage, CreateFileStorage };
